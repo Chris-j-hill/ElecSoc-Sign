8c3cb651150ab2f3b24aa7df406ad59a2a0eaae4
Chris Hill <chris.j.hill94@gmail.com>
2017-04-19 16:29:34 +0100

    Mega files added

---
+++ testshapes_32x64_copy2/communications.ino
@@ -0,0 +1,486 @@
+
+
+/*
+   Communication protocol for retrieving data from arduino due (or other master control board)
+
+   Note that since a tweet is at most 140 characters long, the primary communication data,
+   the max frame size will be slightly more than this 150 bytes. For annimations or bitmap pictures
+   the data will need to be transmitted in multiple blocks. It can be specified in the header data
+   what type of data it is and if there are multiple blcoks to be recieved
+
+   Protocol based on a frame structure as such:   start byte, frame size byte, data identifier, data, ..., checksum byte, endbyte
+
+   The start and end bytes are the characters [ and ] respectively (ascii codes 91 and 93)
+   The frame size is the length of characters in the sring being transmitted
+   The data identifier is the specific purpose the data is intended for, an attempt to future proof the design (text, annimations, debug instruction etc)
+   The data then is the string, or other data to be transmitted
+   The checksum is the difference of 256 and sum of the values of the data modulo 256, to find errors the summed data  and checksum at the reciever should equal zero
+*/
+
+void init_i2c(){
+  
+  Wire.begin(ADDRESS);
+  Wire.onReceive(receiveEvent);
+  pinMode(Request_text_pin, OUTPUT);
+   
+  str.reserve(max_frame_size-HEADER_LENGTH-TRAILER_LENGTH);  // reserve space for string,works without but could override variables when extending string
+  str = "No input string found - No input string found";  // setup string, need to recieve data from master first
+  textMin = str.length() * (-text_width*text_size) - index1;
+  delay(500);     // delay to allow all other arduinos to boot up, make sure bufferes are ready to recieve before starting comunications 
+  
+  update_frame = true;        //request 1 frame on startup
+}
+
+void get_frame(){
+  
+  //request data
+  if (Serial.available() > 0) {   //check for serial input from user
+    if (Serial.peek() == 'a')
+    {
+      Serial.read();
+      Serial.println(Wire.read());    //check for values stored in i2c buffer, useful for analysing interrupt
+      //Serial.read();
+      Serial.println(str);          //print current string
+      Serial.flush();
+    }
+    else if (Serial.peek() == 'r')
+    {
+      Serial.read();
+      str = "No input string found, No input string found";   //setup string, need to recieve data from master first  
+    }
+    else {
+      flag2 = Serial.read();
+      Serial.flush();
+      while (Serial.available() > 0)
+      {
+        char c = Serial.read();      // dump any extra characters eg '\n' so loop isn't run excess number of times
+      }
+    } 
+  }
+  else if(update_frame){      // if code elsewhere requires new frame, eg if animation has finished scrolling
+    Serial.println(F("code triggered request"));
+    flag2=1;
+  }
+
+  
+
+
+  if ( flag2 != 0 || errors != 0 || bad_frame == true) {               //if there has been serial input from user, or non zero erros from last frame, get new frame
+    
+    //Wire.flush();                   //flush anything in buffer, shouldnt be anything but to be safe
+    digitalWrite(Request_text_pin, HIGH);          //request data from master
+    Serial.println(micros());
+    timeprev = millis();
+    timeout = 0;
+
+    while (flag1 == 0 && timeout < 5000) { //wait until interrupt changes flag1 to 1 or timeout
+      delayMicroseconds(100);
+      timeout = millis() - timeprev;
+      //Serial.println(flag1);
+      //Serial.println("waiting for data");
+    }
+
+
+      digitalWrite(Request_text_pin, LOW);  // Finished requesting data
+      delay(10);
+    if (flag1 == 0) {                       // if flag1 is 0 here, timeout occured,stop requesting data then do nothing
+      
+      new_frame = false;                    // code requests new frame until new_frame true, ie no errors
+      Serial.println(F("Break"));
+    }
+    else                                    //otherwise data has arrived
+    {
+     error_check();
+    }
+  }
+} 
+      
+
+
+      
+void error_check(){         // function to error check frame, common to requested frame and non requested frame.
+                            // on requested frame with errors should trigger get_frame() function  
+                                    
+  int frame_length1 = x[FRAME_LENGTH_BYTE];             //the frame length, text plus header plus trailer
+  
+  if (debug){                                       //display some details
+    Serial.print(F("Frame length value (x[1]):"));
+    Serial.println(frame_length1);
+  
+    Serial.print(F("Print frame: "));      //print entire frame for user
+    for (int k = 0; k < frame_length1; k++)
+    {
+      Serial.print((char)x[k]);
+    }
+    Serial.println(F(""));
+
+  }
+  flag1 = 0;                            //reset flag1 for next itteration
+
+  Serial.println (F("Check for Errors"));  //check for errors
+  
+  for (int error_counter = HEADER_LENGTH; error_counter < frame_length1 -1; error_counter++)    // (<frame_length1 -1) as the end byte was not included in the checksum and zero addressing
+  {
+    errors = errors + x[error_counter];
+    //Serial.print(x[error_counter]);
+  }
+  errors = errors % 256;              //should equal zero if arrived correctly
+
+  if (debug) {
+    Serial.print(F("errors present: "));
+    Serial.println(errors);
+    //      for (int j=0; j<=frame_length1; j++){
+    //        Serial.print((char) x[j]);
+    //        }
+    Serial.println(F(""));
+  }
+  
+  
+  if (errors == 0){
+    if (debug)
+    Serial.println(F("frame recieved correctly"));
+    new_frame = true;                             //indicate new frame ready to unpack
+    
+  }
+  bad_frame = false;
+  flag2 = 0;                             //reset for next itteration
+  flag1 = 0;
+  Wire.flush();
+}
+
+
+
+
+void receiveEvent(int bytes)                   //interrupt to read in new string
+{
+  int timeOut2 = millis();
+  frame_length = 150;
+  if (debug){
+    Serial.println(F("data recieved, interupt triggered"));
+    Serial.println(F("data intended for this arduino"));
+  }
+  i = 0;
+  while (flag1 == 0)
+  {
+    if (Wire.available() > 0)
+    {
+      
+      x[i] = Wire.read();                         // read one character from the I2C
+      if (debug)
+        Serial.print((char) x[i]);
+
+      //extract info from frame
+      if (i == FRAME_LENGTH_BYTE)                                //frame size byte, entire frame including heading and trailer bytes
+      {
+        frame_length = x[i];
+        //Serial.println((byte)frame_length);
+      }
+      
+      if (i == DATA_IDENTIFIER_BYTE)                                //frame type (mode) byte, used to decied what to do with recieved data
+      {
+        frame_type = x[i];
+//        if (frame_type==68){
+//        frame_length = frame_length-1;
+//        Serial.println("@@@@");
+//        }
+      }
+
+      // exit conditions when full frame recieved
+
+      if (i == frame_length-1)                      //end of frame reached, set flag high, allow wait loop to exit
+      {
+        flag1 = 1;                                // use to exit waitng loop
+        i = 0;                                    // reset i
+        frame_length = max_frame_size;            // set frame length to default length for next iteration
+        if (debug){
+//          Serial.println(F(""));
+//          Serial.println(F("Frame recieved"));
+        }
+        Wire.flush();
+      }
+      else                        //otherwise increment i
+      {
+        i++;
+      }
+    }
+
+    else flag1=1;
+    
+//    else
+//    {
+//      Serial.print("Did not Receive full frame (available = ");
+//      Serial.print(Wire.available());
+//      Serial.println(")");
+//      bad_frame = true;             // set as bad frame
+//      flag1=1;                      // break from while loop
+//      
+//      if (millis() - timeOut2 > 1000)
+//      {
+//        Serial.println("time out");
+//        break;
+//      }
+//    }
+
+    if (flag1==1 && digitalRead(Request_text_pin) != HIGH&& !bad_frame){     // full frame recieved but not requested, call function
+                                                                
+      flag1=0;      //set back to zero to avoid confusion
+      
+      error_check();                                            // should do error checking and if errors present, get_frame() will be called
+      break;                                                          // if no errors frame will be unpacked as usual
+    }
+  }
+  
+  //Serial.println("exit interrupt");
+  Wire.flush();
+}
+
+
+void unpack_frame(){    //if new frame arrived sucessfully extract relevent information form it
+
+  if(new_frame){      //only unpack frame if new frame is recieved with no errors
+    Serial.println(F("new frame arrived, unpack frame"));
+    int temp1 = (x[DATA_IDENTIFIER_BYTE])-48;  //define frame type, could be mode of operation or nature of frame data
+    
+    Serial.print(F("frame type: "));
+    Serial.println(temp1);
+    
+    if (temp1 < 20){        // change mode to new mode (0-19 define valid modes of operation, ie scrolling, static text etc)
+      if (mode!= temp1){              // if current mode does not equal temp1
+        mode_change_time = millis();  // log time of mode change, used to display current mode on right most screen  
+      }
+      mode = temp1;   //change mode
+	    matrix.Mode(mode2matrixMode());   //update screen operating mode
+	  
+      int e =0; // x[FRAME_LENGTH_BYTE-2];
+
+      Serial.print(F("New string: "));
+      for(e = HEADER_LENGTH; e < x[FRAME_LENGTH_BYTE]-TRAILER_LENGTH;e++){
+        str[e-HEADER_LENGTH] = x[e];                  //copy frame into string, offset by frame header length
+        
+        Serial.print((char)x[e]);
+      }
+      Serial.println(F(""));
+//      Serial.print("E = ");
+//      Serial.println(e-HEADER_LENGTH);
+      //str[e-HEADER_LENGTH] = NULL;
+      str.remove(x[FRAME_LENGTH_BYTE]-HEADER_LENGTH-TRAILER_LENGTH);        //remove any data left after the current string length
+      Serial.println(x[FRAME_LENGTH_BYTE]-HEADER_LENGTH-TRAILER_LENGTH);
+      textMin = (x[FRAME_LENGTH_BYTE]-HEADER_LENGTH-TRAILER_LENGTH) * (-text_width*text_size) - index1;
+      Serial.println(textMin);
+      Serial.println(str);
+    }
+    else if (temp1 == 20){       // 20 is brightness scaling value*100 (to make into int for transmission)
+      current_light_scale = (float)x[HEADER_LENGTH]/ 100;  //first data element, offset by header length 
+      Serial.println(F("brightness frame arrived"));
+    }
+
+    else if (temp1 > 20 && temp1 < 40){   // this mode indicates to display mode data for a couple seconds befroe normal operation
+      
+    }
+    
+    
+    
+  }
+  update_frame = false;   //new frame arrived, stop requesting update for now
+  new_frame = false;      // do not attempt to unpack more than once 
+  textMin = str.length() * (-text_width*text_size) - index1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// _____________________________________________________
+// ______________________  old code ____________________
+// _____________________________________________________
+
+
+/*
+void read_text() {
+  
+  int flag2 =0;   //keep requesting newest frame until one arrives with no errors
+  int frame_counter=0;
+  bool arrived = false;
+  bool mem_allocated = false;
+  byte *temp_array;
+  int temp_array_length=0;
+  
+  
+  do{
+    digitalWrite(Request_text_pin, HIGH);   //request data
+    flag1=0;    //indicator when frame arrived, changes in interupt 
+
+    if (debug)
+    Serial.println("requested data, waiting for response");
+    
+    while (flag1 == 0) {    
+      //wait until new frame read in
+    }
+    
+    if (debug)
+    Serial.println("frame recieved");
+  
+    //if frame indicates multiple frames to come, create storage array and loop, do error checking later 
+    
+    if (frame_type > 100 && frame_type < 256 && frame_counter != frame_type-100){ // 100 indicated multiple frames, value from frame_type -100 indicates the number of related frames to arrive
+      if (debug){
+          Serial.println("indicates multiple frames");
+          Serial.print("frames remaining: ");
+          Serial.println(frame_type-100);
+      }
+      
+      if( frame_counter ==0 ){    //first frame, allocate memory
+        int Size1 = (frame_type-100)*(max_frame_size); //allocate memory equal to the size of total data to be reciever
+        temp_array =  (byte *) malloc(Size1);
+        mem_allocated = true;
+      }
+  
+      //copy data into temp_array;
+      for (int w=0; w<frame_length-4;w++){
+      temp_array[frame_counter*145+w] = x[w+3];   //copy data data elements and checksums into temp array
+      }   
+      temp_array_length = temp_array_length + (frame_length-5); //counter to track how long the data is for error checking
+      frame_counter++;    //increment to next frame
+    
+      if (frame_type-100 == frame_counter){  //all frames arrived
+      arrived == true;
+      frame_counter=0;    //reset in case errors have occured and need to loop again
+      }
+    
+      
+    }
+    else {          //else if standard frame type set arrived to true after one frame
+    arrived =true;
+    temp_array = &x[0];   //set temp array as address of x[0] for error checking
+    if (debug)
+    Serial.println("all data recieved, start error checking");
+    }
+  
+    //if arrived do error checking, else loop without error checking for speed, check later
+    if (arrived==true){
+      //acknowledge recieved frame, disconnect comm
+      digitalWrite(Request_text_pin, LOW);
+      delay(1);
+      //check_for errors
+      
+      //if no errors present, the sum of the data and checksum modulo 256 should equal 0
+      
+      int errors =0;
+      for (int error_counter =3; error_counter<frame_length-1; error_counter++){
+        errors = errors + x[error_counter];
+      }
+      errors = errors%256;
+
+      if (debug){
+      Serial.print("errors present: ");
+      Serial.println(errors);
+      for (int j=0; j<frame_length; j++){
+        Serial.print((char) x[j]);
+        }
+        Serial.println("");
+      }
+      
+      //if errors present, repeat entire process, request again
+      if (errors != 0){
+        flag2=0;    //confirm it will loop
+      }
+      else if (errors ==0){  
+        flag2=1;   //otherwise exit
+      }
+    }
+  }
+  while(flag2==0);   //loop until entire data recieved with no errors
+
+  if (debug)
+  Serial.println("frame recieved with no errors");
+
+  
+  //if no errors, copy data to relevent location
+
+    //case of scrolling text string
+    if (frame_type == 1){
+    int k = 3;      //start at initial data element
+    while (k <= frame_length-2) { //copy all data up to frame length
+      str[k] = (uint8_t)x[k];     //cast required 
+      k++;
+      }
+    }
+
+    //else if (frame_type ==2)    //vertical scroll
+    //new_string =1   //include this to identify if lines array needs updating
+    //...
+      
+    
+    if (mem_allocated ==true){    //if memory was allocated in recieving this frame, deallocate
+       free(temp_array);
+    }
+}
+
+
+
+
+void receiveEvent(int bytes) {    //interrupt to read in new string
+  if (debug)
+  Serial.println("data recieved, interupt triggered");
+  
+  //if (digitalRead(Enable_com)== HIGH){    //confirm the character is for this board
+    if (debug)
+    Serial.println("data for this arduino");
+    
+    x[i] = Wire.read();    // read one character from the I2C
+    
+    //extract info from frame
+    if (i == 1) { //frame size byte, entire frame including heading and trailer bytes
+      frame_length = x[i];
+    }
+  
+    if (i == 2) { //frame type byte, used to decied what to do with recieved data
+      frame_type = x[i];
+    }
+
+   //exit conditions when full frame recieved
+  
+   if (i == frame_length) {    //end of frame reached, det flag high, allow wait loop to exit
+      flag1 = 1;
+      i = 0;          //reset i
+      frame_length = max_frame_size; // set frame length to default length for next iteration
+    }
+    else {                       //otherwise increment i
+      i++;
+    }
+  //}  
+}
+
+
+
+void check_serial(){            //check for serial data input
+  String input_str = "";
+  
+  if (Serial.available()>0){
+    int i =0;
+      while(Serial.available()>0){
+        input_str = Serial.readString();
+        i++;
+        
+      }
+      if (input_str == "debug"){
+        debug = !debug;
+      }
+      
+      if (debug){
+        Serial.println(input_str);
+        
+      }
+   
+  }
+}
+*/
---
+++ testshapes_32x64_copy2/testshapes_32x64_copy2.ino
@@ -0,0 +1,275 @@
+// testshapes demo for RGBmatrixPanel library.
+// Demonstrates the drawing abilities of the RGBmatrixPanel library.
+// For 32x64 RGB LED matrix.
+
+// NOTE THIS CAN ONLY BE USED ON A MEGA! NOT ENOUGH RAM ON UNO!
+
+
+#include <Adafruit_GFX.h>   // Core graphics library
+#include <RGBmatrixPanel.h> // Hardware-specific library, modified for this project
+#include <Wire.h>
+
+//define functions here
+void home_balls();
+void bouncing_balls();
+void scroll_text_horizontal();
+void scroll_text_vertical();
+void update_lines_array();
+
+void check_serial();
+void read_text();
+void receiveEvent(int bytes);
+
+/*  Define the screen number based on the layout below
+    ________________ ________________ ________________ ________________
+   |                |                |                |                |
+   |       0        |       1        |       2        |       3        |
+   |                |                |                |                |
+   |________________|________________|________________|________________|
+
+*/
+//        \/ address now set in hardware
+//#define ADDRESS 0                   // change for each arduino 1/2/3/4/RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
+int hardware_address_pin[2] = {44,45};
+
+
+//#define SCREEN_NUM 1        // the position of this matrix in the screen, based on above, easier to index if zero
+#define NUM_SCREENS  4              // the total number of matrices in setup
+#define TOT_WIDTH  64*NUM_SCREENS   // the number of leds across the total screen width
+#define TOT_HEIGHT 32               // height of the screen
+
+#define HEADER_LENGTH 3             // start byte, frame length, data identifier/mode
+#define TRAILER_LENGTH 2
+#define DATA_IDENTIFIER_BYTE 2      // address in frame of data identifier 
+#define FRAME_LENGTH_BYTE 1         // address of frame length byte, length of entire frame
+
+
+
+// matrix pins
+#define OE1  8
+#define OE2  9
+#define LAT 10
+#define CLK 11
+#define A   A0
+#define B   A1
+#define C   A2
+#define D   A3
+
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE1, OE2, true, 64);
+
+//syncing matrices
+int ADDRESS = 0;                  // hardware configured address
+int SCREEN_NUM =0;                // default to 0, change immediately in set_address function
+int trig = 13;                    // pin to update display, essentially a clock from arduino due
+int index1 = 64 * SCREEN_NUM;      // define offset of the left most pixel on this matrix from left most pixel of the overall screen
+int flag = 0;                     // used to perform edge detection on trig signal
+int Reset = 5;                    // reset all objects to initial positions, ground signal when connected, floating otherwise
+int counter = 0;                  // helps for scrolling, counts the number fo pulses form the due
+bool state = true;                // when state of trig flips, run main loop
+//int mode =1;                    //define mode as scrolling, can be changed
+bool debug = true;
+int mode_change_time =0;
+
+
+//text general variables
+int dist_from_top =8;   // change by changing mode
+int text_size = 2;       // also change through mode change
+int text_height = 9;  // sizes uses for manual wrapping text
+int text_width = 6;   // height = n*8 and wight = n*6 where n = text_size variable used in setTextSize method
+uint16_t text_colour = 0x0000;    // colour zero, means use changing hue function, otherwise display data colour
+const int max_lines = 140/(TOT_WIDTH/(6*8));       //(max tweet size)/(screen width / max char size)
+int num_lines = max_lines;
+
+//define matrix for scrolling horizontal
+String   str = "No input string found - No input string found";       //string for horizontal scrolling, defualt message to allert no recieved string
+int    textX   = matrix.width(),              //start pos, just off screen, need to include waiting conditions later
+       textMin = str.length() * (-text_width*text_size) - index1,   //allow to scroll off left side by this much
+       hue     = 0;
+
+
+//define matrix for scrolling vertical
+char   vert_str[] = "No input text found";
+int    textY   = matrix.height(),                       //start pos, just off screen, need to include waiting conditions later
+       textMinY = -(text_size*text_height)*max_lines,                        //allow to scroll off top by this much, Change in function based on correct sizes
+       hueY     = 0,
+       new_string =0;
+//       
+//typedef struct {                                    // structure of individual lines for vertical scrolling, 
+//                                                    // create array of these for overall string
+//  int valid = 0;                                    // indicator for whether this line is active
+//  char line[(TOT_WIDTH/6)+1] = "line not filled";   // create char array of max size plus one which will be used for endline char  
+//} manual_wrap;
+//
+//static manual_wrap lines[max_lines];    //create array of lines
+//
+
+
+
+
+//Ball stuff
+
+//initialise ball positions
+int8_t ball[3][4] = {
+  {  3,  0,  1,  1 }, // Initial X,Y pos & velocity for 3 bouncy balls
+  { 17, 15,  1, -1 }, //start x,y off screen based on index
+  { 27,  4, 1,  1 }
+};
+//set ball colours
+static const uint16_t PROGMEM ballcolor[3] = {
+  0x0080, // Green=1
+  0x0002, // Blue=1
+  0x1000  // Red=1
+};
+
+
+//communications
+// i2c pins on mega: sda = 20   scl = 21
+int Request_text_pin = 7;
+//int Enable_com = 23; 
+
+int flag1 = 0;      // do error checking once frame arrived, flag set high when arrives
+byte flag2 = 0;
+int i = 0;          // counter for frame size
+
+//byte frame_length;  // frame length as extracted form incoming signal
+byte frame_type;    //frame type defines what is to be done with info, ie where string should go or other operation
+byte frame_length = 150;    //length of current frame
+int max_frame_size = 150;    //max frame size 150, ie max length of a tweet plus some frame info
+byte x[150];
+
+int errors = 0;             //number of errors found
+bool bad_frame = false; 
+bool new_frame = false;     //new frame arrived marker for unpack_frame function
+bool reset_flag = false;
+int mode=1;                  //current mode of operation
+bool update_frame = false;  //request variable to allow code to request updates
+//int mode_change_time =0;
+int timeout = 0;
+int timeprev = 0;
+int prev_loop=0;
+
+float current_light_scale = 1.0;
+bool mega_wait = true;
+
+void setup() {
+  Serial.begin(115200);
+  
+  set_address();    //function to set the address of the mega based on hardware config
+
+  
+  Serial.print(F("Address: "));
+  Serial.println(ADDRESS);
+
+
+  
+  Serial.flush();
+  // timing clock pins
+  pinMode(trig, INPUT);   //clock signal to increment step
+  pinMode(Reset, INPUT);  //start scrolling at same time
+
+  Serial.println(F("Begin wait"));
+  while(!digitalRead(Reset)){}    //wait for reset pulse
+  Serial.println(F("Due ready, begin startup"));
+  
+  //matrix initialisation
+  Serial.println(F("Matrix Setup"));
+  matrix.begin();
+  matrix.setTextWrap(false); // Allow text to run off right edge
+  matrix.setTextSize(text_size);
+  matrix.Mode(mode2matrixMode()); //set operating mode of matrix
+  //home_balls();               //set ball positions
+  
+  Serial.println(F("Matrix Setup Done"));
+
+  Serial.println(F("Comunications Setup"));
+     init_i2c();
+     get_frame();
+     unpack_frame();
+  Serial.println(F("Comunications Setup Done"));
+  Serial.print(F("String to display: "));
+  Serial.println(str);
+ 
+  prev_loop= millis();
+  Serial.println(F("Start Loop"));
+}
+
+void loop() {
+  //delay(1000);
+
+  if (state != digitalRead(trig)){ // || millis()>prev_loop+50) {  //edge detection of trig pin, trig on every edge
+    state = digitalRead(trig);
+    if (debug){
+//      Serial.print("Mode: ");
+//      Serial.println(mode);
+    }
+   
+    //prev_loop= millis();
+    //if (NUM_SCREENS == SCREEN_NUM-1 && millis() < mode_change_time+2000){   //if this is the right most screen and there was a mode change less than 2 seconds ago
+      //mode_change_display();            //display mode change details
+    //}
+    //else{                               //otherwise do normal procedure
+      // Clear background
+      matrix.fillScreen(0);
+  
+      flag = 1;
+      //bouncing_balls();   //update ball positions
+      if (mode == 1)
+      scroll_text_horizontal();      //update text positions
+  
+     // else if (mode==2)
+     // scroll_text_vertical();
+  
+      
+      flag = 0;
+      // Update display
+      matrix.swapBuffers(false);    //swap buffers, write to one display the other
+      //Serial.println(str);
+    //}
+  }
+
+  //no need to do comms on trig, might aswell use the dead time
+  get_frame();
+  unpack_frame();
+
+}
+
+int mode2matrixMode(){   // fucntion to take the operatin mode and return the mode for the matrices, ie two screens on, one on one off, or both off
+  Serial.println(F("Change screen mode"));
+  if (mode == 1 || mode ==21) // or other double screen modes...
+      return 2;           //both on, is mode 2 in matrix.mode() mehtod
+  else if (mode == 2 || mode ==22)  // or other single screen modes...
+      return 1;
+  else if (mode==3 || mode==23)
+      return 3;
+}
+
+
+void set_address(){
+  pinMode(hardware_address_pin[1], INPUT);
+  pinMode(hardware_address_pin[2], INPUT);
+  
+  int temp1 = digitalRead(44);
+  int temp2 = digitalRead(45);
+
+
+  if (temp1 == LOW && temp2 == LOW){
+  ADDRESS = 1;
+  SCREEN_NUM=0;
+  }
+  else if (temp1 == LOW && temp2 == HIGH){
+  ADDRESS = 2;
+  SCREEN_NUM=1;
+  }
+  else if (temp1 == HIGH && temp2 == LOW){
+  ADDRESS = 3;
+  SCREEN_NUM=2;
+  }
+  else if (temp1 == HIGH && temp2 == HIGH){
+  ADDRESS = 4;
+  SCREEN_NUM=3;
+  }
+
+  index1 = 64 * SCREEN_NUM;
+}
+
+
